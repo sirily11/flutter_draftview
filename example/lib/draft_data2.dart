c8fb20e0d23e4a1a1afed14b0756f06a3fd3f634
sirily11 <sirily1997@gmail.com>
2021-01-18 12:08:08 +0800

    merge branch

---
+++ example/lib/draft_data2.dart
@@ -0,0 +1,476 @@
+var data2 = {
+  "blocks": [
+    {
+      "key": "99gfe",
+      "text": "创造102，是一档专注于女性选秀的综艺节目。",
+      "type": "blockquote",
+      "depth": 0,
+      "inlineStyleRanges": [
+        {"offset": 0, "length": 22, "style": "BOLD"}
+      ],
+      "entityRanges": [],
+      "data": {}
+    },
+    {
+      "key": "97u36",
+      "text": "第一部分",
+      "type": "header-one",
+      "depth": 0,
+      "inlineStyleRanges": [
+        {"offset": 0, "length": 4, "style": "BOLD"}
+      ],
+      "entityRanges": [],
+      "data": {}
+    },
+    {
+      "key": "2mlhr",
+      "text":
+          "主持人： 欢迎各位年轻貌美的女性来到我们创造102的比赛现场。经过这次的激烈较量，选手们能够脱颖而出最后成为一个由24人组成的BKB48（Best king bitch 48) 组合。每个能进入决赛圈的选手，都会受到我们专业的导师的指导，出道。",
+      "type": "unstyled",
+      "depth": 0,
+      "inlineStyleRanges": [
+        {"offset": 0, "length": 4, "style": "BOLD"}
+      ],
+      "entityRanges": [],
+      "data": {}
+    },
+    {
+      "key": "4hqr6",
+      "text": "",
+      "type": "unstyled",
+      "depth": 0,
+      "inlineStyleRanges": [],
+      "entityRanges": [],
+      "data": {}
+    },
+    {
+      "key": "2teh1",
+      "text": "各位导师：谢谢，你们都是最棒的女生，要相信自己。我也相信你们。祝好运。",
+      "type": "unstyled",
+      "depth": 0,
+      "inlineStyleRanges": [
+        {"offset": 0, "length": 5, "style": "BOLD"}
+      ],
+      "entityRanges": [],
+      "data": {}
+    },
+    {
+      "key": "5ej6s",
+      "text": "",
+      "type": "unstyled",
+      "depth": 0,
+      "inlineStyleRanges": [],
+      "entityRanges": [],
+      "data": {}
+    },
+    {
+      "key": "dm5mt",
+      "text": "",
+      "type": "unstyled",
+      "depth": 0,
+      "inlineStyleRanges": [],
+      "entityRanges": [],
+      "data": {}
+    },
+    {
+      "key": "73p86",
+      "text":
+          "主持人：好了，话不多说，开始缩圈！各位选手，降落伞准备！那么第一个从天而降的选手是———— 超琪灿。那么有请超琪灿选手向我们讲述她的故事。开始跳伞！",
+      "type": "unstyled",
+      "depth": 0,
+      "inlineStyleRanges": [
+        {"offset": 0, "length": 4, "style": "BOLD"}
+      ],
+      "entityRanges": [],
+      "data": {}
+    },
+    {
+      "key": "b90mo",
+      "text": "",
+      "type": "unstyled",
+      "depth": 0,
+      "inlineStyleRanges": [],
+      "entityRanges": [],
+      "data": {}
+    },
+    {
+      "key": "ak6ur",
+      "text": "",
+      "type": "unstyled",
+      "depth": 0,
+      "inlineStyleRanges": [],
+      "entityRanges": [],
+      "data": {}
+    },
+    {
+      "key": "9hr2b",
+      "text":
+          "超琪灿：大家好，我是一号选手超琪灿。我的老家在贵州省的一个偏僻的小村庄。今天也是我第一天来到这个大城市，参加这么豪华的节目。非常兴奋。因为今年新冠疫情影响，我的老家的一所中学终于是顶不住开支，要废校了。那是我们全村唯一的一所学校，虽然破旧但是也是我们40多名同学的共同回忆。学校要拆了，我舍不得我的老师，以及陪伴了我那么多年的课桌。虽然学校破旧，但是我仍然记得同学们一起奔跑在学校跑道上的快乐时光。虽然学校破旧，但是我们每一天都过的很充实，很快乐。虽然学校破旧，但是我依然不会忘记放学后一起去学校对面的咖啡厅喝那边的美味的咖啡。然而，这份快乐却不在了，同学们过去三年的母校就要离开了。就像一位失去的老朋友一样分别，我们满眼都是舍不得的眼泪。我。。。 我。。。。 而且因为我们的村庄很是偏僻，这所学校一旦被拆迁，其他的学生每天都要越过崇山峻岭去到县城的一所高中上课。来来回回的要十几公里。很多学生，尤其是那些家境不富裕的同学，哪怕是下雪天也只能穿着破旧的鞋子走着严峻的山路，很是辛苦。所以我想，我如果能够把握住这次机会，我要让更多的人听到我们的故事，也希望能够因此攒到足够多的钱，帮助母校，重建校园！下面是我们全校师生一起写的歌曲，歌名叫《扬帆起航》。",
+      "type": "unstyled",
+      "depth": 0,
+      "inlineStyleRanges": [
+        {"offset": 0, "length": 4, "style": "BOLD"}
+      ],
+      "entityRanges": [],
+      "data": {}
+    },
+    {
+      "key": "61c21",
+      "text": "",
+      "type": "unstyled",
+      "depth": 0,
+      "inlineStyleRanges": [],
+      "entityRanges": [],
+      "data": {}
+    },
+    {
+      "key": "a0uos",
+      "text": "歌曲：",
+      "type": "unstyled",
+      "depth": 0,
+      "inlineStyleRanges": [
+        {"offset": 0, "length": 3, "style": "BOLD"}
+      ],
+      "entityRanges": [],
+      "data": {}
+    },
+    {
+      "key": "dsm0h",
+      "text": "一起唱",
+      "type": "unstyled",
+      "depth": 0,
+      "inlineStyleRanges": [
+        {"offset": 0, "length": 3, "style": "ITALIC"}
+      ],
+      "entityRanges": [],
+      "data": {}
+    },
+    {
+      "key": "85c6h",
+      "text": "嘿 嘿 嘿 起航！",
+      "type": "unstyled",
+      "depth": 0,
+      "inlineStyleRanges": [
+        {"offset": 0, "length": 9, "style": "ITALIC"}
+      ],
+      "entityRanges": [],
+      "data": {}
+    },
+    {
+      "key": "edvul",
+      "text": "嘿 嘿 嘿 起航！",
+      "type": "unstyled",
+      "depth": 0,
+      "inlineStyleRanges": [
+        {"offset": 0, "length": 9, "style": "ITALIC"}
+      ],
+      "entityRanges": [],
+      "data": {}
+    },
+    {
+      "key": "5adfg",
+      "text": "阳光田野下生长的花儿",
+      "type": "unstyled",
+      "depth": 0,
+      "inlineStyleRanges": [
+        {"offset": 0, "length": 10, "style": "ITALIC"}
+      ],
+      "entityRanges": [],
+      "data": {}
+    },
+    {
+      "key": "e2pc6",
+      "text": "望着蓝天满心欢笑",
+      "type": "unstyled",
+      "depth": 0,
+      "inlineStyleRanges": [
+        {"offset": 0, "length": 8, "style": "ITALIC"}
+      ],
+      "entityRanges": [],
+      "data": {}
+    },
+    {
+      "key": "10jml",
+      "text": "吹着风 我们心在眺望 翱翔",
+      "type": "unstyled",
+      "depth": 0,
+      "inlineStyleRanges": [
+        {"offset": 0, "length": 13, "style": "ITALIC"}
+      ],
+      "entityRanges": [],
+      "data": {}
+    },
+    {
+      "key": "5sa77",
+      "text": "",
+      "type": "unstyled",
+      "depth": 0,
+      "inlineStyleRanges": [],
+      "entityRanges": [],
+      "data": {}
+    },
+    {
+      "key": "ddhvd",
+      "text": "",
+      "type": "unstyled",
+      "depth": 0,
+      "inlineStyleRanges": [],
+      "entityRanges": [],
+      "data": {}
+    },
+    {
+      "key": "6ij5d",
+      "text":
+          "主持人：刚刚听完了超琪灿选手的故事，想必大家都感同身受了吧。那种为同学，为学校找想的夙愿大家一定都感受到了吧。相信这位同学一定能够在这创造102的舞台上大放异彩。那么接下来有请我们的二号选手笔踏蚕空降，开始跳伞！",
+      "type": "unstyled",
+      "depth": 0,
+      "inlineStyleRanges": [
+        {"offset": 0, "length": 4, "style": "BOLD"}
+      ],
+      "entityRanges": [],
+      "data": {}
+    },
+    {
+      "key": "10d1v",
+      "text": "",
+      "type": "unstyled",
+      "depth": 0,
+      "inlineStyleRanges": [],
+      "entityRanges": [],
+      "data": {}
+    },
+    {
+      "key": "dchjq",
+      "text": "",
+      "type": "unstyled",
+      "depth": 0,
+      "inlineStyleRanges": [],
+      "entityRanges": [],
+      "data": {}
+    },
+    {
+      "key": "44ggm",
+      "text":
+          "笔踏蚕：听，远处的风声，看近处的花儿。四季在变化，我依然相信你会回来。大家好我是二号选手笔踏蚕。我要讲述的是一个名为等待的故事。我还清楚的记得那是一个阴雨绵绵的日子。我和我的青梅竹马一起去云南旅游。起初的那几天每天都很快乐。能和他在一起玩可能是我这辈子最高兴的事情。但是没想到突如其来的意外打断了我们原有的安排。那天我们在丽江古镇，本来约定一同去吃那里的酸奶，但是在拐角处他看了一眼远处之后对我说让我先去。他随后就来，之后我就在那家酸奶店一等就是一年。在这一年里我每天都打他的电话，但是没有人接，问他家他回去没有也没有音讯。唯一的线索就是他走之前留下的那句“我们酸奶店见”。我了解他，知道他不会食言，所以我就这样边在酸奶店等边去古镇寻找他的线索。为了赚生活费我跟酸奶店店长说能不能在这打工，没想到店长很爽快的答应了。在这里我过的很开心，还认识到了个很风趣的小男孩，他每天鼓励我。但是时间依旧是让我很憔悴，越到后面，我就越容易流眼泪。有时候到深夜会一个人嚎啕大哭。有一天，那个小男孩说，你这一辈子会忘记那么多事情，何不忘记这件呢？之后，我就没见过他了。直到有一天，我听到邻居说，找到了我的亲梅竹马。我过去一看。原来是他的尸体。在尸体旁边留着一张纸，上面写着。“忘记这件事情吧”。我才意识到原来他一直就在这，但是因为一些原因不能相见。而怕我一直流泪选择终结自己的生命来让我结束这没有结局的等待。但是傻瓜，我怎么可能忘记得了你。讲了这么多我想为大家献上一首《忘记吧》。",
+      "type": "unstyled",
+      "depth": 0,
+      "inlineStyleRanges": [],
+      "entityRanges": [],
+      "data": {}
+    },
+    {
+      "key": "45k52",
+      "text": "",
+      "type": "unstyled",
+      "depth": 0,
+      "inlineStyleRanges": [],
+      "entityRanges": [],
+      "data": {}
+    },
+    {
+      "key": "acm7q",
+      "text": "",
+      "type": "unstyled",
+      "depth": 0,
+      "inlineStyleRanges": [],
+      "entityRanges": [],
+      "data": {}
+    },
+    {
+      "key": "7ifgc",
+      "text": "歌曲：",
+      "type": "unstyled",
+      "depth": 0,
+      "inlineStyleRanges": [
+        {"offset": 0, "length": 3, "style": "BOLD"}
+      ],
+      "entityRanges": [],
+      "data": {}
+    },
+    {
+      "key": "6r8la",
+      "text": "忘记吧 忘记吧",
+      "type": "unstyled",
+      "depth": 0,
+      "inlineStyleRanges": [
+        {"offset": 0, "length": 7, "style": "ITALIC"}
+      ],
+      "entityRanges": [],
+      "data": {}
+    },
+    {
+      "key": "di6d1",
+      "text": "等待没有结果啊",
+      "type": "unstyled",
+      "depth": 0,
+      "inlineStyleRanges": [
+        {"offset": 0, "length": 7, "style": "ITALIC"}
+      ],
+      "entityRanges": [],
+      "data": {}
+    },
+    {
+      "key": "a9qui",
+      "text": "忘记吧 忘记吧",
+      "type": "unstyled",
+      "depth": 0,
+      "inlineStyleRanges": [
+        {"offset": 0, "length": 7, "style": "ITALIC"}
+      ],
+      "entityRanges": [],
+      "data": {}
+    },
+    {
+      "key": "c3g33",
+      "text": "结果不必在唱",
+      "type": "unstyled",
+      "depth": 0,
+      "inlineStyleRanges": [
+        {"offset": 0, "length": 6, "style": "ITALIC"}
+      ],
+      "entityRanges": [],
+      "data": {}
+    },
+    {
+      "key": "bb5et",
+      "text": "这场情 或许痛心",
+      "type": "unstyled",
+      "depth": 0,
+      "inlineStyleRanges": [
+        {"offset": 0, "length": 8, "style": "ITALIC"}
+      ],
+      "entityRanges": [],
+      "data": {}
+    },
+    {
+      "key": "323po",
+      "text": "但我不后悔",
+      "type": "unstyled",
+      "depth": 0,
+      "inlineStyleRanges": [
+        {"offset": 0, "length": 5, "style": "ITALIC"}
+      ],
+      "entityRanges": [],
+      "data": {}
+    },
+    {
+      "key": "btim0",
+      "text": "因为我始终都爱着你",
+      "type": "unstyled",
+      "depth": 0,
+      "inlineStyleRanges": [
+        {"offset": 0, "length": 9, "style": "ITALIC"}
+      ],
+      "entityRanges": [],
+      "data": {}
+    },
+    {
+      "key": "4asjn",
+      "text": "",
+      "type": "unstyled",
+      "depth": 0,
+      "inlineStyleRanges": [],
+      "entityRanges": [],
+      "data": {}
+    },
+    {
+      "key": "5h4o",
+      "text": "",
+      "type": "unstyled",
+      "depth": 0,
+      "inlineStyleRanges": [],
+      "entityRanges": [],
+      "data": {}
+    },
+    {
+      "key": "a8lin",
+      "text":
+          "主持人：真是让人痛苦的回忆。扣人心的歌声再加上泪目的故事，让我们来到了二号笔踏蚕选手的世界中去。感受到了那份回忆。接下来的空投选手一定能带来更精彩的故事，哦不 歌声。有请三号选手乔吉喰。开始跳伞！",
+      "type": "unstyled",
+      "depth": 0,
+      "inlineStyleRanges": [
+        {"offset": 0, "length": 4, "style": "BOLD"}
+      ],
+      "entityRanges": [],
+      "data": {}
+    },
+    {
+      "key": "2pf3k",
+      "text": "",
+      "type": "unstyled",
+      "depth": 0,
+      "inlineStyleRanges": [],
+      "entityRanges": [],
+      "data": {}
+    },
+    {
+      "key": "5s698",
+      "text": "",
+      "type": "unstyled",
+      "depth": 0,
+      "inlineStyleRanges": [],
+      "entityRanges": [],
+      "data": {}
+    },
+    {
+      "key": "56vc2",
+      "text": "",
+      "type": "unstyled",
+      "depth": 0,
+      "inlineStyleRanges": [],
+      "entityRanges": [],
+      "data": {}
+    },
+    {
+      "key": "7m4ge",
+      "text":
+          "乔吉喰：喵，五喵，秒喵。大家好我是乔吉喰，是一名咖啡蛋糕店看板喵娘。小时候我被父母抛弃。很久很久都被遗忘在街道的角落里。每天喝着露水，吃着垃圾。生活没有未来。有一天我和我的姐姐都生病了，在下雨的时候萎缩在街灯的电线杆下。突然一名男性出现，他发现了我们，之后带我们回到他父亲经营的咖啡店。让我们住下喵。他每天都给我们新鲜的食物，还有温暖的家。我们姐妹俩长大了，也想着帮他做一点事情喵。于是就成为了咖啡店的看板喵娘。每一天都很开心，快乐。直到有一天，事情发生了意外。我们的救命恩人——那位男生说是要和青梅竹马一起去云南旅游，结果就这样消失了整整一年。之后有个人好不容易，告诉了我们一些关于他的新闻，结果竟然是他已经死掉的消息。我们很是伤心喵。令我们姐妹更没有想到的事情是，因为疫情，我们这家在学校门口的咖啡店都， 半年没有生意。终于，半年后，要和对门的学校一起关门了。但是我们没有放弃，因为曾经在那么绝望的时候，有一位如此温暖的人类帮助了我们。所以我相信奇迹一定会再次降临。有一天，我看到对面的学校的学生一起，召集起来准备出道当女团，报名了创造102来拯救学校，于是我和我的姐妹喵也决定要搏一搏，希望能够拯救那个曾经帮我们姐妹喵的咖啡店，希望那个我们曾经都爱的主人留下的唯一的回忆，能继续常存下去。",
+      "type": "unstyled",
+      "depth": 0,
+      "inlineStyleRanges": [
+        {"offset": 0, "length": 4, "style": "BOLD"}
+      ],
+      "entityRanges": [],
+      "data": {}
+    },
+    {
+      "key": "33n26",
+      "text": "",
+      "type": "unstyled",
+      "depth": 0,
+      "inlineStyleRanges": [],
+      "entityRanges": [],
+      "data": {}
+    },
+    {
+      "key": "eo4g5",
+      "text": "",
+      "type": "unstyled",
+      "depth": 0,
+      "inlineStyleRanges": [],
+      "entityRanges": [],
+      "data": {}
+    },
+    {
+      "key": "5btcp",
+      "text": "主持人：故事很精彩，我相信你们俩姐妹的心意一定能够传承到那位男生那去的！好的稍事休息，我们待会继续空投。开始跳伞！",
+      "type": "unstyled",
+      "depth": 0,
+      "inlineStyleRanges": [
+        {"offset": 0, "length": 4, "style": "BOLD"}
+      ],
+      "entityRanges": [],
+      "data": {}
+    },
+    {
+      "key": "o1r4",
+      "text": "",
+      "type": "header-three",
+      "depth": 0,
+      "inlineStyleRanges": [],
+      "entityRanges": [],
+      "data": {}
+    }
+  ],
+  "entityMap": {}
+};
---
+++ example/lib/views/Demo2.dart
@@ -0,0 +1,43 @@
+import 'package:draft_view/draft_view.dart';
+import 'package:example/draft_data.dart';
+import 'package:example/draft_data2.dart';
+import 'package:example/post_settings_data.dart';
+import 'package:flutter/material.dart';
+
+class Demo2 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Theme(
+        data: ThemeData(
+          textTheme: TextTheme(
+            headline1: TextStyle(fontSize: 30, color: Colors.black),
+            headline2: TextStyle(fontSize: 25, color: Colors.black),
+            headline3: TextStyle(fontSize: 20, color: Colors.black),
+            bodyText1: TextStyle(height: 2, fontSize: 17),
+          ),
+        ),
+        child: Scrollbar(
+          child: SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: DraftView(
+                rawDraftData: data2,
+                plugins: [
+                  TextPlugin(),
+                  BlockQuotePlugin(),
+                  HeaderPlugin(),
+                  ImagePlugin(),
+                  PostSettingsPlugin(rawSettings: settings),
+                  ListPlugin(),
+                  AudioPlugin(),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
