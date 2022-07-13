ff2c2c7ca3d6e4031ce81a6f3ce84827e6379bf6
王赛鹏 <wangsaipeng_727@sina.com>
2022-07-13 11:06:48 +0800

    Fallback code

--- lib/draft_view/block/blocks/header_block.dart
+++ lib/draft_view/block/blocks/header_block.dart
@@ -1,3 +1,4 @@
+import 'dart:convert';
 import 'package:draft_view/draft_view/block/base_block.dart';
 import 'package:flutter/material.dart';
 
@@ -44,31 +45,124 @@ class HeaderBlock extends BaseBlock {
   @override
   TextStyle renderStyle(BuildContext context) {
     var prevStyle = super.renderStyle(context);
-    TextStyle headeline;
-    var textTheme = Theme.of(context).textTheme;
-    if (level == 1) {
-      headeline = textTheme.headline1!;
-    } else if (level == 2) {
-      headeline = textTheme.headline2!;
-    } else if (level == 3) {
-      headeline = textTheme.headline3!;
-    } else if (level == 4) {
-      headeline = textTheme.headline4!;
-    } else if (level == 5) {
-      headeline = textTheme.headline5!;
-    } else {
-      headeline = textTheme.headline6!;
+
+    switch (level) {
+      case 1:
+        var textStyle = Theme.of(context).textTheme.headline1!.copyWith(
+              fontWeight: prevStyle.fontWeight,
+              fontStyle: prevStyle.fontStyle,
+              decoration: prevStyle.decoration,
+              color: textColor(context),
+            );
+        return textStyle;
+
+      case 2:
+        var textStyle = Theme.of(context).textTheme.headline2!.copyWith(
+              fontWeight: prevStyle.fontWeight,
+              fontStyle: prevStyle.fontStyle,
+              decoration: prevStyle.decoration,
+              color: textColor(context),
+            );
+
+        return textStyle;
+
+      case 3:
+        var textStyle = Theme.of(context).textTheme.headline3!.copyWith(
+              fontWeight: prevStyle.fontWeight,
+              fontStyle: prevStyle.fontStyle,
+              decoration: prevStyle.decoration,
+              color: textColor(context),
+            );
+        return textStyle;
+      case 4:
+        var textStyle = Theme.of(context).textTheme.headline4!.copyWith(
+              fontWeight: prevStyle.fontWeight,
+              fontStyle: prevStyle.fontStyle,
+              decoration: prevStyle.decoration,
+              color: textColor(context),
+            );
+        return textStyle;
+      case 5:
+        var textStyle = Theme.of(context).textTheme.headline5!.copyWith(
+              fontWeight: prevStyle.fontWeight,
+              fontStyle: prevStyle.fontStyle,
+              decoration: prevStyle.decoration,
+              color: textColor(context),
+            );
+        return textStyle;
+      default:
+        var textStyle = Theme.of(context).textTheme.headline6!.copyWith(
+              fontWeight: prevStyle.fontWeight,
+              fontStyle: prevStyle.fontStyle,
+              decoration: prevStyle.decoration,
+              color: textColor(context),
+            );
+        return textStyle;
     }
-    return headeline.copyWith(
-      fontWeight: prevStyle.fontWeight,
-      fontStyle: prevStyle.fontStyle,
-      decoration: prevStyle.decoration,
-      color: textColor(context),
-    );
   }
 
   @override
   InlineSpan render(BuildContext context, {List<InlineSpan>? children}) {
     return TextSpan(text: textContent, style: renderStyle(context));
   }
+}
+
+class RawDraftEntityT {
+  final Map<String, dynamic> data;
+  final String mutability;
+  final String type;
+
+  RawDraftEntityT({
+    required this.data,
+    required this.mutability,
+    required this.type,
+  });
+
+  RawDraftEntityT copyWith({
+    Map<String, dynamic>? data,
+    String? mutability,
+    String? type,
+  }) =>
+      RawDraftEntityT(
+        data: data ?? this.data,
+        mutability: mutability ?? this.mutability,
+        type: type ?? this.type,
+      );
+
+  factory RawDraftEntityT.fromRawJson(String str) =>
+      RawDraftEntityT.fromJson(json.decode(str));
+
+  String toRawJson() => json.encode(toJson());
+
+  factory RawDraftEntityT.fromJson(Map<String, dynamic> json) =>
+      RawDraftEntityT(
+        data: Map.from(json["data"])
+            .map((k, v) => MapEntry<String, dynamic>(k, v)),
+        mutability: json["mutability"],
+        type: json["type"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "data": Map.from(data).map((k, v) => MapEntry<String, dynamic>(k, v)),
+        "mutability": mutability,
+        "type": type,
+      };
+}
+
+enum ComposedEntityType { IMAGE, LINK, PHOTO, TOKEN }
+
+enum CoreDraftBlockType {
+  ATOMIC,
+  BLOCKQUOTE,
+  CODE_BLOCK,
+  HEADER_FIVE,
+  HEADER_FOUR,
+  HEADER_ONE,
+  HEADER_SIX,
+  HEADER_THREE,
+  HEADER_TWO,
+  ORDERED_LIST_ITEM,
+  PARAGRAPH,
+  UNORDERED_LIST_ITEM,
+  UNSTYLED
 }
